38a70ce36888946adc107d49b493844fef85cd1c
woshiwanting <1370348686@qq.com>
2012-12-31 01:08:01 -0800

    更新文件

---
+++ src/yas/ajax/yas.ajax.js
@@ -0,0 +1,25 @@
+/**
+ * @fileOverview yas Ajax方法
+ * @fileOverview http://www.w3school.com.cn/ajax/ajax_xmlhttprequest_create.asp
+ * @author wt
+ * @version 1.0
+ * @since 2012-12-31
+ */
+var yas = yas || {};
+
+yas.ajax = {
+	/**
+	 * 创建XMLHttpRequest
+	 */
+	getXHR : function() {
+		if(XMLHttpRequest == undefined) {
+			XMLHttpRequest = function() {
+				return new ActiveXObject(
+					navigator.userAgent.indexOf('MSIE 5') > 0 ?
+					'Microsoft.XMLHTTP' : 'Msxml2.XMLHTTP'
+				);
+			};
+		}
+		return new XMLHttpRequest();
+	}
+};

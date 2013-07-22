235aeb56486b694625840184af22994cf9f22bb3
woshiwanting <1370348686@qq.com>
2013-07-21 22:18:25 -0700

    1.完善常用方法杰克丹尼

--- demo/accumulation/accumulation.js
+++ demo/accumulation/accumulation.js
@@ -420,5 +420,52 @@ var yas = {
 		//原因分析：1号位置的this指代的是那个对象，在with执行的过程中，会先在所指的对象中找，所有 x = 20,修改的是 x : 10的值,
 		//y = 30，修改的是foo中的y，在bar中调用的时候，x, y分别指代的是foo中的局部变量，this.x是对象中的x，foo中的x只是声明
 		//未赋值,注明：with会被删除
+		
+		//demo，在我们去搜寻__parent__之前，首先会去__proto__的链接中
+		Object.prototype.x = 10;
+ 
+		var w = 20;
+		var y = 30;
+		 
+		// 在SpiderMonkey全局对象里
+		// 例如，全局上下文的变量对象是从"Object.prototype"继承到的
+		// 所以我们可以得到“没有声明的全局变量”
+		// 因为可以从原型链中获取
+		 
+		console.log(x); // 10
+		 
+		(function foo() {
+		 
+		  // "foo" 是局部变量
+		  var w = 40;
+		  var x = 100;
+		 
+		  // "x" 可以从"Object.prototype"得到，注意值是10哦
+		  // 因为{z: 50}是从它那里继承的
+		 
+		  with ({z: 50}) {
+		    console.log(w, x, y , z); // 40, 10, 30, 50
+		  }
+		 
+		  // 在"with"对象从作用域链删除之后
+		  // x又可以从foo的上下文中得到了，注意这次值又回到了100哦
+		  // "w" 也是局部变量
+		  console.log(x, w); // 100, 40
+		 
+		  // 在浏览器里
+		  // 我们可以通过如下语句来得到全局的w值
+		  console.log(window.w); // 20
+		 
+		})();	
+		
+		//demo
+		var x = 10, y = 10; 
+		with ({x: 20}) { 
+		  var x = 30, y = 30; 
+		  console.info(x); // 30 
+		  console.info(y); // 30 
+		} 
+		console.info(x); // 10 
+		console.info(y); // 30
 	}
 };
